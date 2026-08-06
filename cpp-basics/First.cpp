f123d813511c8337f2c1e7b706dbb5263c887098
Nofal Ali <nofilkachhot@gmail.com>
2026-08-06 18:36:41 +0530

    data types and variable declaration in c++

--- cpp-basics/First.cpp
+++ cpp-basics/First.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world"<<endl;
-    cout << "hello how are you...?? \n";
-    cout << 5+2;
+    // cout << "hello world"<<endl;
+    // cout << "hello how are you...?? \n";
+    // cout << 5+2;
+
+    // * data types and variable declaration.
+
+    // int a = 5, b = 10;
+    // cout << a + b;
+
+    // char c = 'a';
+    // cout << c;
+
+    // float f = 1.23;
+    // cout << f;
+
+    // double d = 2.654634543;
+    // cout << d;
+
+    // bool b = true;
+    // cout << b;
 }
