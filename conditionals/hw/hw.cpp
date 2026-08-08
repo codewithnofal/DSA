5c8dc53277497deed16770ac984a7916ecd48387
Nofal Ali <nofilkachhot@gmail.com>
2026-08-08 19:56:33 +0530

    solve some if else basics q

---
+++ conditionals/hw/hw.cpp
@@ -0,0 +1,44 @@
+// 1.Two numbers are given, print the bigger number, It is given that both numbers can’t be the same.
+
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter Number a: ";
+//     cin >> a;
+//     cout << "Enter Number b: ";
+//     cin >> b;
+
+//     if (a > b)
+//     {
+//         cout << a << " is big";
+//     }
+//     else if (b > a)
+//     {
+//         cout << b << " is big";
+//     }
+//     else
+//     {
+//         cout << "both numbers are same";
+//     }
+// }
+
+// 2. Age of a person is given, print Adult if his/her age is greater than 18, otherwise print Teenager.
+
+int main()
+{
+    int age;
+    cout << "Enter Your Age: ";
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "Adult";
+    }
+    else
+    {
+        cout << "Teenager";
+    }
+}
