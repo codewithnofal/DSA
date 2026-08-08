150ea97457ee9316245150495c3fbb963e8b4223
Nofal Ali <nofilkachhot@gmail.com>
2026-08-08 23:28:33 +0530

    solves some loops questions

--- conditionals/hw/loops.cpp
+++ conditionals/hw/loops.cpp
@@ -28,17 +28,38 @@ using namespace std;
 
 // 3. print number which is devisable by 4.
 
-int main()
+// int main()
 
+// {
+//     int n;
+//     cout << "Enter Number: ";
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (i % 4 == 0)
+//         {
+//             cout << i << endl;
+//         }
+//     }
+// }
+
+// 4. print a to z.
+
+// int main()
+// {
+//     char alpha;
+//     for (alpha = 'a'; alpha <= 'z'; alpha++)
+//     {
+//         cout << alpha << " ";
+//     }
+// }
+
+// 5. print reverse numbers;
+
+int main()
 {
-    int n;
-    cout << "Enter Number: ";
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 10; i >= 1; i--)
     {
-        if (i % 4 == 0)
-        {
-            cout << i << endl;
-        }
+        cout << i << endl;
     }
 }
