eb51ca3eee6364ffca896ad9fb256635159618a1
Nofal Ali <nofilkachhot@gmail.com>
2026-08-11 12:40:31 +0530

    solving some advance q of loop

---
+++ loops/loops.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// 1.calculate power of number.
+
+// int main()
+// {
+//     int n, i, pow, num;
+
+//     cout << "enter your number: ";
+//     cin >> n;
+//     cout << "enter a power: ";
+//     cin >> pow;
+
+//     num = n;
+
+//     for (i = 1; i < pow; i++)
+//     {
+//         num *= n;
+//     }
+//     cout << num;
+// }
+
