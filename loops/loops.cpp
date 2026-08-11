acac0200c33313de2a1606bfdb8cb7e37f4fa8b3
Nofal Ali <nofilkachhot@gmail.com>
2026-08-11 14:23:02 +0530

    solve some practice q

--- loops/loops.cpp
+++ loops/loops.cpp
@@ -21,3 +21,45 @@ using namespace std;
 //     cout << num;
 // }
 
+// int main()
+// {
+//     int n, i;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     if (n < 2)
+//     {
+//         cout << "Not Prime";
+//         return 0;
+//     }
+//     else
+//     {
+//         for (i = 2; i < n; i++)
+//         {
+//             if (n % i == 0)
+//             {
+//                 cout << "Not Prime";
+//                 return 0;
+//             }
+//         }
+//         cout << "Prime";
+//         return 0;
+//     }
+// }
+
+// fibonacci num.
+
+int main()
+{
+    int n, i, curr, last = 0, prev = 1;
+    cout << "Enter a Number: ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        curr = prev + last;
+        last = prev;
+        prev = curr;
+    }
+    cout << last;
+}
