9a3998af0726ac4c9c39e9b18e1fc44830c38c68
Nofal Ali <nofilkachhot@gmail.com>
2026-08-12 20:51:57 +0530

    solve one q of pattern programing

--- loops/pattern-programing/pattern.cpp
+++ loops/pattern-programing/pattern.cpp
@@ -7,8 +7,10 @@ int main()
 
     for (i = 1; i <= 5; i++)
     {
-        for(j=0; j<=5; j++){
-            
+        for (j = 1; j <= 5; j++)
+        {
+            cout << "*" << " ";
         }
+        cout << endl;
     }
 }
