b99e8840a4aacf4237b5b2dea6a63b5a8896e898
Nofal Ali <nofilkachhot@gmail.com>
2026-08-13 12:37:08 +0530

    solve some advavance q of pattern programing

--- loops/pattern-programing/pattern.cpp
+++ loops/pattern-programing/pattern.cpp
@@ -56,3 +56,62 @@ using namespace std;
 //     }
 // }
 
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 1; i <= 5; i++)
+//     {
+//         for (j = 5; j >= 1; j--)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 1; i <= 5; i++)
+//     {
+//         char name = 'a' + i - 1;
+//         for (j = 1; j <= 5; j++)
+//         {
+//             cout << name << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            char name = 'a' + j -1;
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    int i;
+    char name;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (name = 'a'; name <= 'e'; name++)
+        {
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+}
