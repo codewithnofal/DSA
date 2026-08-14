7fbac6e86fa9f966e805d02e630b42535b926140
Nofal Ali <nofilkachhot@gmail.com>
2026-08-14 23:13:27 +0530

    solved some another q

--- loops/pattern-programing/hw/hw.cpp
+++ loops/pattern-programing/hw/hw.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 1; i <= 5; i++)
+//     {
+//         for (j = 1; j <= 5; j++)
+//         {
+//             cout << 4 << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 1; i <= 5; i++)
+//     {
+//         for (j = 1; j <= 5; j++)
+//         {
+//             cout << j * j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int i, j;
+
+//     for (i = 1; i <= 5; i++)
+//     {
+//         for (j = 1; j <= 6; j++)
+//         {
+//             cout << j * j * j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
 int main()
 {
-    int i, j;
+    int i;
+    char j;
 
     for (i = 1; i <= 5; i++)
     {
-        for (j = 1; j <= 5; j++)
+
+        for (j = 'F'; j <= 'K'; j++)
         {
-            cout << 4 << " ";
+            cout << j << " ";
         }
         cout << endl;
     }
