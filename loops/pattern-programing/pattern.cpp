dfa96750d7e2002f0f9d380935617138668ae265
Nofal Ali <nofilkachhot@gmail.com>
2026-08-19 12:55:56 +0530

    solve 2 q of pattern printing

--- loops/pattern-programing/pattern.cpp
+++ loops/pattern-programing/pattern.cpp
@@ -175,6 +175,27 @@ using namespace std;
 //     }
 // }
 
+// int main()
+// {
+//     int row, col, n;
+
+//     cout << "Enter a Number: ";
+//     cin >> n;
+
+//     for (row = 1; row <= 5; row++)
+//     {
+//         for (col = 1; col <= n - row; col++)
+//         {
+//             cout << "  ";
+//         }
+//         for (col = 1; col <= 2 * row - 1; col++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
 int main()
 {
     int row, col, n;
@@ -182,15 +203,19 @@ int main()
     cout << "Enter a Number: ";
     cin >> n;
 
-    for (row = 1; row <= 5; row++)
+    for (row = 1; row <= n; row++)
     {
         for (col = 1; col <= n - row; col++)
         {
             cout << "  ";
         }
-        for (col = 1; col <= 2 * row - 1; col++)
+        for (col = 1; col <= row; col++)
         {
-            cout << "* ";
+            cout << col << " ";
+        }
+        for (col = row - 1; col >= 1; col--)
+        {
+            cout << col << " ";
         }
         cout << endl;
     }
