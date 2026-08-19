f3b4626e25732405bfa62f2286f567fbac1ce43d
Nofal Ali <nofilkachhot@gmail.com>
2026-08-19 15:19:37 +0530

    make some changes

--- loops/pattern-programing/pattern.cpp
+++ loops/pattern-programing/pattern.cpp
@@ -196,27 +196,29 @@ using namespace std;
 //     }
 // }
 
-int main()
-{
-    int row, col, n;
+// int main()
+// {
+//     int row, col, n;
 
-    cout << "Enter a Number: ";
-    cin >> n;
+//     cout << "Enter a Number: ";
+//     cin >> n;
 
-    for (row = 1; row <= n; row++)
-    {
-        for (col = 1; col <= n - row; col++)
-        {
-            cout << "  ";
-        }
-        for (col = 1; col <= row; col++)
-        {
-            cout << col << " ";
-        }
-        for (col = row - 1; col >= 1; col--)
-        {
-            cout << col << " ";
-        }
-        cout << endl;
-    }
-}
+//     for (row = 1; row <= n; row++)
+//     {
+//         for (col = 1; col <= n - row; col++)
+//         {
+//             cout << "  ";
+//         }
+//         for (col = 1; col <= row; col++)
+//         {
+//             cout << col << " ";
+//         }
+//         for (col = row - 1; col >= 1; col--)
+//         {
+//             cout << col << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
