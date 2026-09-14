e3a0d5feb901f0284dd9fb91e055728069f03ef8
Nofal Ali <nofilkachhot@gmail.com>
2026-09-14 21:19:31 +0530

    Add C++ build task and implement multiple pattern printing functions

--- pattern-printing/patterns.cpp
+++ pattern-printing/patterns.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
 using namespace std;
 
+void pattern1(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void pattern2(int n)
+
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void pattern3(int n)
+
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern4(int n)
+
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern5(int n)
+{
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<)
+    }
+}
+
 int main()
 {
     int n;
+    cout << "Enter a Number: ";
     cin >> n;
-    cout << n;
+    pattern4(n);
 }
