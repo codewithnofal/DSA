0ebea60e8f1daed82aa432f6106df6e7e0757eda
Nofal Ali <nofilkachhot@gmail.com>
2026-09-15 22:56:17 +0530

    Add pattern11 and pattern12 functions; update main to call pattern12

--- pattern-printing/patterns.cpp
+++ pattern-printing/patterns.cpp
@@ -131,6 +131,48 @@ void pattern10(int n){
 }
 
 
+void pattern11(int n){
+    int start = 1;
+    for(int i=0; i<n; i++){
+        
+        if(i%2==0) start = 1;
+        else start = 0;
+        for(int j=0; j<=i; j++){
+            cout <<start << " ";
+            start = 1-start;
+        }
+        cout << endl;
+    }
+}
+
+
+void pattern12(int n){
+    int space = 2 * (n-1);
+    
+    for(int i=1; i<=n; i++){
+
+        // number
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+
+        // space
+        for(int j=1; j<=space; j++){
+            cout << " ";
+        }
+        
+        // number
+
+        for(int j=i; j>=1; j--){
+            cout << j;
+        }
+        
+        cout << endl;
+        space -= 2;
+    }
+
+    
+}
 
 
 
@@ -140,5 +182,5 @@ int main()
     int n;
     cout << "Enter a Number: ";
     cin >> n;
-    pattern10(n);
+    pattern12(n);
 }
