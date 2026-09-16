d19fd3ade941a85a6654edb67945c001a1dc8662
Nofal Ali <nofilkachhot@gmail.com>
2026-09-16 11:59:51 +0530

    Add pattern function for pyramid printing; update main to call pattern

--- pattern-printing/patterns.cpp
+++ pattern-printing/patterns.cpp
@@ -215,10 +215,36 @@ void pattern16(int n){
     }
 }
 
+void pattern(int n){
+    int space = 2*(n-1);
+        for(int i=0; i<n; i++){
+            // digit
+            for(int j=1; j<=i; j++){
+                cout << j;
+            }
+
+            // space
+            for(int j=1; j<=space; j++){
+                cout << " ";
+            }
+
+            // digit
+             for(int j=i; j>=1; j--){
+                cout << j;
+            }
+            cout << endl;
+            space -= 2;
+        }
+}
+
+
+
+
+
 int main()
 {
     int n;
     cout << "Enter a Number: ";
     cin >> n;
-    pattern16(n);
+    pattern(n);
 }
